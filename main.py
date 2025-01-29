7c22110f4ab386f1d1bf1bf300b5becc7854286d
Lucas <iscario@tlen.pl>
2025-01-29 20:32:32 +0100

    rotating player

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pygame 
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
